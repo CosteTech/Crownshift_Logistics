ed82b9bffa0a3e46a5b58fde73cec22df4138504
njoroge015 <collinsnjoroge973@gmail.com>
2026-02-27 20:48:17 +0300

    feat: Add Firebase Admin SDK initialization and utilities for authentication, Firestore, and admin email management.

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -2,9 +2,9 @@ import "server-only";
 import * as admin from "firebase-admin";
 
 const requiredEnvVars = {
-  FIREBASE_ADMIN_PROJECT_ID: process.env.FIREBASE_ADMIN_PROJECT_ID,
-  FIREBASE_ADMIN_CLIENT_EMAIL:process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  FIREBASE_ADMIN_PROJECT_ID: "crownshift-logistics",//FIREBASE_ADMIN_PROJECT_ID,
+  FIREBASE_ADMIN_CLIENT_EMAIL: "firebase-adminsdk-fbsvc@crownshift-logistics.iam.gserviceaccount.com",//process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  FIREBASE_ADMIN_PRIVATE_KEY: "",//FIREBASE_ADMIN_PRIVATE_KEY,
 };
 
 if (process.env.NODE_ENV === "production") {
